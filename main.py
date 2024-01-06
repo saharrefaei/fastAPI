cffcf7e67d29e7b70ca4d3bef029e0aa3b61253c
Sahar Refaei <saharrefaei1999@gmail.com>
2024-01-06 16:53:08 +0100

    feat : pathing the parameters into the api

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
 # im able to add any routes depends on my project requeiremnts
-
+ 
 @app.get("/" , description='This is my first get api')
 async def root():
     return {"hi , you are in the root get / "}
@@ -11,3 +12,31 @@ async def root():
 @app.post("/")
 async def root():
     return {"hi , you are in the root post / "}
+
+
+@app.get('/users')
+async def users():
+        return {"hi , you are in the root users /users "}
+
+
+@app.get("/users/{users_id}")
+async def usersId(users_id : int):
+        return {"hi , you are in the root users /users " : {users_id}}
+
+class FoodCategoryEnum (str,Enum):
+    candy =  'candy'
+    fruits = 'fruits'
+    water = 'water'
+    
+@app.get('/Foodmasurment/{foodName}')
+async def Get_food(foodName :FoodCategoryEnum ):
+    if foodName ==FoodCategoryEnum.candy :
+        return {"hi , you are in a " :foodName  }
+    elif foodName ==FoodCategoryEnum.fruits :
+        return {"hi , you are in a " :foodName  }
+    elif foodName ==FoodCategoryEnum.water :
+        return {"hi , you are in a " :foodName  }
+    
+  
+
+    
